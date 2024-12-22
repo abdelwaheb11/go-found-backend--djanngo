84f7cbe5d7d05f2e2cf6c6f497fcb681c22042fd
abdelwaheb11 <yazidiabdelwaheb@gmail.com>
2024-12-22 13:31:54 +0100

    allow origin all

--- go_found/settings.py
+++ go_found/settings.py
@@ -70,10 +70,13 @@ MIDDLEWARE = [
 
 ALLOWED_HOSTS = ['*'] 
 
-CORS_ALLOWED_ORIGINS = [
-    'https://abdelwaheb11.github.io',
-    'http://localhost:4200'
-]
+#CORS_ALLOWED_ORIGINS = [
+#   'https://abdelwaheb11.github.io',
+#   'http://localhost:4200'
+#]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 CORS_ALLOW_CREDENTIALS = True
 
