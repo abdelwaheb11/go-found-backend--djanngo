7a45437076a5d4de83f8dfee3f58f8d8a586d57f
abdelwaheb11 <yazidiabdelwaheb@gmail.com>
2024-12-23 00:44:14 +0100

    Fix wsgi module issue

--- go_found/settings.py
+++ go_found/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$n@5j--i^684(5*7m+(2p)2=ss9@g9cpv22#n5f3p*(_b3q&*z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://gofound.up.railway.app']
 
