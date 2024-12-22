428ebe1331c2306ba0c46c691f4fd3623ca0c435
abdelwaheb11 <yazidiabdelwaheb@gmail.com>
2024-12-22 22:14:09 +0100

    allow all cors origin

--- go_found/settings.py
+++ go_found/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
 
 ROOT_URLCONF = 'go_found.urls'
